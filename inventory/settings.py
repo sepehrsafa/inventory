7879c2edcf52f1bd0f65a942c28d689c40fe3bf7
Sepehr Safa <safasepehr@gmail.com>
2022-01-19 10:13:58 -0500

    changed setting file, debug=False

--- inventory/settings.py
+++ inventory/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3qfd1ri#97$wcv!&!xh$5ate9-m9mug*s$a74d=*#1dow1z54^'
+SECRET_KEY = 'wqqwq121!@!3qfd1ri#97$wcv!&!xh$5ate9-m9mug*s$a74d=*#1dow1z54^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['inventory-sepehr.herokuapp.com','.localhost', '127.0.0.1', '[::1]']
 
